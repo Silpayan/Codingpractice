87b75e0c5a9f99d0b916a8fc17a6bbcbdf29379c
Silpayan Dutta <60825821+Silpayan@users.noreply.github.com>
2021-09-20 19:30:25 -0700

    Add files via upload

--- LeetCode/search/MeetingRoomsII.java
+++ LeetCode/search/MeetingRoomsII.java
@@ -18,7 +18,48 @@ public class MeetingRoomsII {
 		System.out.println(new MeetingRoomsII().minMeetingRooms(intervals));
 	}
 	
-    public int minMeetingRooms(int[][] intervals) {
+	public int minMeetingRooms(int[][] intervals) {
+	    
+        //sort the meetings
+        
+        Arrays.sort(intervals, new Comparator<int[]>(){
+                                        @Override
+                                        public int compare(int a[], int b[]){
+                                            return a[0]-b[0];
+                                        }
+                                    });
+            
+        
+        //System.out.println(intervals);
+        //start a meeting by iterating over them
+        int room = 0;
+        PriorityQueue<Integer> endTime = new PriorityQueue<>();
+        
+        
+        for(int interval[]:intervals){
+            endTime.offer(interval[1]);
+            //System.out.println("Size : "+endTime.size());
+            
+            if(interval[0]>=endTime.peek()){
+                endTime.poll();
+            }
+                
+            /*if(interval[0]<endTime.peek()) {
+                room++;
+            } else{
+                endTime.poll();
+            }*/
+            
+        }
+        
+        //push the end time in a PQ as we can remove when checking next meeting time if this became avail/prev meeting got over
+            
+            return endTime.size();
+    
+    }
+    
+        
+    public int minMeetingRoomsLC(int[][] intervals) {
         if(intervals == null || intervals.length == 0) return 0;
         Arrays.sort(intervals, new Comparator<int[]>(){
             @Override
@@ -27,7 +68,7 @@ public class MeetingRoomsII {
             }
         });
         PriorityQueue<Integer> endTime = new PriorityQueue<>();
-        int room = 0;oi
+        int room = 0;
         for(int i = 0; i < intervals.length; i++){
             endTime.offer(intervals[i][1]);
             if(intervals[i][0] < endTime.peek())  room++;
@@ -37,5 +78,4 @@ public class MeetingRoomsII {
         }
         return room;
     }
-
 }
