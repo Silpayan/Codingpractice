b97b4ba1bc8db35bf79636d92491400b7706beba
Silpayan Dutta <60825821+Silpayan@users.noreply.github.com>
2024-03-03 10:46:53 -0800

    Update MinimumKnightMoves_Premium.java
    adding problem statement

--- leetcode/bfs_dfs/MinimumKnightMoves_Premium.java
+++ leetcode/bfs_dfs/MinimumKnightMoves_Premium.java
@@ -2,6 +2,15 @@ package leetcode.bfs_dfs;
 
 import java.util.*;
 
+/*
+1197. Minimum Knight Moves
+In an infinite chess board with coordinates from -infinity to +infinity, you have a knight at square [0, 0].
+
+A knight has 8 possible moves it can make, as illustrated below. Each move is two squares in a cardinal direction, then one square in an orthogonal direction.
+
+Return the minimum number of steps needed to move the knight to the square [x, y].  It is guaranteed the answer exists.
+
+*/
 public class MinimumKnightMoves_Premium {
 
 	
